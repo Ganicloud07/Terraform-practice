744b540650104a19a643a6c72ab4f7df33aa454c
Gani <ganiarbaaz5@gmail.com>
2026-01-03 16:04:10 +0530

    Day-4

--- Day-4-remote-statefile/backend.tf
+++ Day-4-remote-statefile/backend.tf
@@ -3,5 +3,11 @@ terraform {
     bucket = "gani-07"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+
+    # Enable S3 native locking
+    use_lockfile = true 
+    # The dynamodb_table argument is no longer needed
+    
   }
+
 }
--- Day-4-remote-statefile/main.tf
+++ Day-4-remote-statefile/main.tf
@@ -4,7 +4,7 @@ ami = "ami-00ca570c1b6d79f36"
 instance_type = "t3.micro"
 
 tags = {
-  Name = "dev"
+  Name = "gani"
 }
 
 }

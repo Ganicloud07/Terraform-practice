8e4705fb463b9d85748fd4cd245bfdce6edceb69
Tauseef Gani <ganiarbaaz5@gmail.com>
2026-01-24 18:41:46 +0530

    Update main.tf

--- Day-2-statefile/main.tf
+++ Day-2-statefile/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "name" {
-    ami = "ami-00ca570c1b6d79f36"
+    ami = "ami-0ff5003538b60d5ec"
    instance_type = "t3.micro"
 
 
- /*tags = {
+ tags = {
     Name = "Gani"
    }
-
+/*
  iam_instance_profile = "ec2-tf"
 
  forces replacement means terminated the exiting server and create top of up new server with give subnet 
